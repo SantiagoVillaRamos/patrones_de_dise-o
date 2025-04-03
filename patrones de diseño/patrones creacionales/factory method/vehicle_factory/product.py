7f5f4f4e63863192cd2b867ea3b34c9ee9e6f7b9
santiago <ramossoloyo12345@gmail.com>
2025-04-03 10:45:26 -0500

    agregar archivos de factory method

---
+++ patrones de diseño/patrones creacionales/factory method/vehicle_factory/product.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class Vehiculo(ABC):
+    
+    @abstractmethod
+    def deliver(self):
+        pass
+    
+    
+class Carro(Vehiculo):
+    
+    def __init__(self, model):
+        self._model = model
+    
+    def deliver(self):
+        return f"\n Auto {self._model} entregado"
+    
+    
+class Camion(Vehiculo):
+    
+    def __init__(self, model):
+        self._model = model
+        
+    def deliver(self):
+        return f"\n Camion {self._model} entregado"
+    
+    
